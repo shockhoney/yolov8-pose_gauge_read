8c205405bc9b8964d7af30be1f301593fa957cea
ganjin <ganjin0808@gmail.com>
2026-01-13 10:49:19 +0800

    Update gauge_read.py

--- gauge_read.py
+++ gauge_read.py
@@ -123,7 +123,7 @@ def process_gauge(weights, source, output):
         # 绘图
         cv2.line(img, (int(pt_c[0]), int(pt_c[1])), (int(pt_tip[0]), int(pt_tip[1])), (0, 0, 255), 3)
         cv2.line(img, (int(pt_c[0]), int(pt_c[1])), (int(pt_max[0]), int(pt_max[1])), (0, 0, 255), 2)
-        cv2.line(img, (int(pt_c[0]), int(pt_c[1])), (int(pt_tip[0]), int(pt_tip[1])), (0, 0, 255), 3)
+        cv2.line(img, (int(pt_c[0]), int(pt_c[1])), (int(pt_min[0]), int(pt_min[1])), (255, 0, 0), 2)
         
         cv2.circle(img, (int(pt_min[0]), int(pt_min[1])), 4, (255,0,0), -1) # 蓝点：Min中心
         cv2.circle(img, (int(pt_max[0]), int(pt_max[1])), 4, (0,0,255), -1) # 红点：Max中心
