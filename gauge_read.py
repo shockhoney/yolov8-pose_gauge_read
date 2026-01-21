219f8d82090f00082b2adac09f90bd7b0f7eaafb
shockhoney <ganjin0808@gmail.com>
2026-01-21 13:17:45 +0800

    1

--- gauge_read.py
+++ gauge_read.py
@@ -9,7 +9,7 @@ from paddleocr import PaddleOCR
 # -----------------------------------------------------------
 # 0. 环境初始化
 # -----------------------------------------------------------
-ocr = PaddleOCR(use_angle_cls=True, lang='en')  # 使用paddleocr进行OCR
+ocr = PaddleOCR(use_angle_cls=True, lang='en', use_gpu=False)  # 禁用GPU，强制使用CPU
 CLS_CENTER, CLS_GAUGE, CLS_MAX, CLS_MIN, CLS_TIP = 0, 1, 2, 3, 4
 
 # -----------------------------------------------------------
